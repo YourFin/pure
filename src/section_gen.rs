bbbaf916260db22f1d3a610b20a869458b28bf2d
YourFin <d.junkpobox+git@gmail.com>
2020-03-02 19:32:00 -0800

    Section gen framework

--- src/section_gen.rs
+++ src/section_gen.rs
@@ -1,15 +1,30 @@
+type Utility = i32;
+
 pub trait PromptSectionGenerator {
-	fn max_length() -> i32;
-	fn fg_color() -> termcolor::Color;
-	fn bg_color() -> termcolor::Color;
-	fn gen_section(length: i32);
+    // Potentially relevent: https://www.mit.edu/~modiano/papers/C67.pdf
+    fn utility_for_chars(&self, amount: i32) -> Utility;
+    fn fg_color(&self) -> termcolor::Color;
+    fn bg_color(&self) -> termcolor::Color;
+    fn gen_section(&self, length: i32);
 }
 
-// Takes list of prompt section generators an their precedence, with smaller numbers
-// being higher, and an available amount of prompt to fill
+// Takes list of prompt section generators an their precedence, with smaller
+// numbers being higher, and an available amount of prompt to fill
 //
 // Returns a
-pub fn generate_prompt(prompt_sections: Vec<(i32, &PromptSectionGenerator)>,
-					   length: i32) -> (termcolor::Buffer, termcolor::BufferWriter) {
-	let mut buffer = bufwtr = termcolor::BufferWriter::
+pub fn generate_prompt(
+    prompt_sections: Vec<(i32, &PromptSectionGenerator)>,
+    length: i32,
+) -> (termcolor::Buffer, termcolor::BufferWriter) {
+    let mut bufwtr = termcolor::BufferWriter::stdout(termcolor::ColorChoice::Always);
+    let mut buffer = bufwtr.buffer();
+
+    let prompt_sections_by_precidence = prompt_sections
+        .into_iter()
+        .enumerate()
+        .map(|(idx, (precidence, prompt_sec))| (precidence, idx, prompt_sec))
+        .collect()
+        .sort_unstable_by(|(precidence_a, _, _), (precidence_b, _, _)| {
+            precidence_a.cmp(precidence_b)
+        });
 }
